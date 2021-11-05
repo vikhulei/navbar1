1c0c4534181defb1521fb4c2eba326611c06fa3d
vikhulei <65869915+vikhulei@users.noreply.github.com>
2021-11-05 22:18:47 +0200

    Route Overview created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Overview from "./pages/Overview";
 
 const App = () => {
   return (
     <>
+      <Router>
+        <Routes>
+          <Route path="/overview" element={<Overview />} />
+        </Routes>
+      </Router>
       <h3>Hi</h3>
     </>
   );
