bf773c7524126779085876ce314dd8dd006d0bed
vikhulei <65869915+vikhulei@users.noreply.github.com>
2021-11-05 22:30:11 +0200

    Menubar styling started

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Overview from "./pages/Overview";
 import Menubar from "./components/Menubar";
--- src/components/Menubar.js
+++ src/components/Menubar.js
@@ -1,12 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Nav = styled.nav`
+  height: 60px;
+  background-color: yellow;
+  display: flex;
+  justify-content: right;
+  align-items: center;
+`;
+
+const NavLink = styled(Link)`
+  text-decoration: none;
+  margin-right: 5vw;
+`;
 
 const Menubar = () => {
   return (
     <>
       <Nav>
-        <Link to="/">Home</Link>
-        <Link to="/overview">Overview</Link>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/overview">Overview</NavLink>
       </Nav>
     </>
   );
